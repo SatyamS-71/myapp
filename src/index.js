b4b7aa6d2fb745d6d3b074fb3700d6cbe3bbf1ee
Satyam Singh <96017148+SatyamS-71@users.noreply.github.com>
2025-05-30 14:18:59 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import App from './App.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <div>
-  {/* <App /> */}
-<a href='/workflow'>Go to workflow </a> <p>this should rule out of the possiblity of routing errors</p>
+   <App />
+{/* <a href='/workflow'>Go to workflow </a> <p>this should rule out of the possiblity of routing errors</p> */}
+  
 
 </div>
 );
